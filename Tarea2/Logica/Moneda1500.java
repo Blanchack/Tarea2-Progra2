34d072f8ca96390bcd52b462c17e7df9bc280769
alexb <alexblancor05@gmail.com>
2023-05-12 19:50:00 -0400

    Se añadio la logica de la tarea 1

---
+++ Tarea2/Logica/Moneda1500.java
@@ -0,0 +1,12 @@
+/**
+ * Moneda de 15000
+ */
+class Moneda1500 extends Moneda{
+    public Moneda1500(){
+        super();
+        super.valor = 1500;
+    }
+    public int getValor(){
+        return super.valor;
+    }
+}
---
+++ Tarea2/Logica/Moneda500.java
@@ -0,0 +1,12 @@
+/**
+ * Moneda con valor 500.
+ */
+class Moneda500 extends Moneda{
+    public Moneda500(){
+        super();
+        super.valor = 500;
+    }
+    public int getValor(){
+        return super.valor;
+    }
+}
